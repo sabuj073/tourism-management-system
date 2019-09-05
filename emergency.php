985229bb2fbc9cd570886151bb1e751260412d25
Sabuj <mhsabuj1@gmail.com>
2019-09-05 11:05:47 +0600

    first commit

---
+++ emergency.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['submit2']))
+{
+$pid=intval($_GET['pkgid']);
+$useremail=$_SESSION['login'];
+$fromdate=$_POST['fromdate'];
+$todate=$_POST['todate'];
+$comment=$_POST['comment'];
+$status=0;
+$sql="INSERT INTO tblbooking(PackageId,UserEmail,FromDate,ToDate,Comment,status) VALUES(:pid,:useremail,:fromdate,:todate,:comment,:status)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':pid',$pid,PDO::PARAM_STR);
+$query->bindParam(':useremail',$useremail,PDO::PARAM_STR);
+$query->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
+$query->bindParam(':todate',$todate,PDO::PARAM_STR);
+$query->bindParam(':comment',$comment,PDO::PARAM_STR);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Booked Successfully";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TMS | Package Details</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<link rel="stylesheet" href="css/jquery-ui.css" />
+	<script>
+		 new WOW().init();
+	</script>
+<script src="js/jquery-ui.js"></script>
+					<script>
+						$(function() {
+						$( "#datepicker,#datepicker1" ).datepicker();
+						});
+					</script>
+	  <style>
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>				
+</head>
+<body>
+<!-- top-header -->
+<?php include('includes/header.php');?>
+<div class="banner-3">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Emergency</h1>
+	</div>
+</div>
+<!--- /banner ---->
+<!--- selectroom ---->
+<div class="selectroom">
+	<div class="container">	
+		  <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+<?php 
+	echo "<center><img src='images/police.png'> +8801713615503</center>";
+	echo "<br><br><center><img src='images/hospital.png'>  +880165001506</center>";
+
+?>
+
+
+	</div>
+</div>
+<!--- /selectroom ---->
+<<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,224 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>HOME :: ||</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+    <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="css/banner_style.css" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <!-- font-awesome-icons -->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+
+
+
+   <div id="homepage-slider" class="st-slider">
+            <input type="radio" class="cs_anchor radio" name="slider" id="play1" checked="" />
+            <input type="radio" class="cs_anchor radio" name="slider" id="slide1" />
+            <input type="radio" class="cs_anchor radio" name="slider" id="slide2" />
+            <input type="radio" class="cs_anchor radio" name="slider" id="slide3" />
+            <div class="images">
+                <div class="images-inner">
+                    <div class="image-slide">
+                        <div class="banner-w3pvt-1">
+                            <div class="overlay-w3ls">
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="image-slide">
+                        <div class="banner-w3pvt-2">
+                            <div class="overlay-w3ls">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="image-slide">
+                        <div class="banner-w3pvt-3">
+                            <div class="overlay-w3ls">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="labels">
+                <div class="fake-radio">
+                    <label for="slide1" class="radio-btn"></label>
+                    <label for="slide2" class="radio-btn"></label>
+                    <label for="slide3" class="radio-btn"></label>
+                </div>
+            </div>
+
+
+
+
+
+<!--<<div class="banner">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Tourism Management System</h1>
+	</div>
+</div>!-->
+
+
+<!--<div class="container">
+	<div class="rupes">
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-usd"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO USD. 50 OFF</h3>
+				<h4><a href="offers.html">TRAVEL SMART</a></h4>
+				
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-h-square"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO 70% OFF</h3>
+				<h4><a href="offers.html">ON HOTELS ACROSS WORLD</a></h4>
+				
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-mobile"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>FLAT USD. 50 OFF</h3>
+				<h4><a href="offers.html">US APP OFFER</a></h4>
+			
+			</div>
+				<div class="clearfix"></div> !-->
+		</div>
+	
+	</div>
+</div>
+<!--- /rupes ---->
+
+
+
+
+<!---holiday---->
+<div class="container">
+	<div class="holiday">
+	
+
+
+
+	
+	<h3>Locations</h3>
+
+					
+<?php $sql = "SELECT * from spots";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+			<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->image);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<br><h4><?php echo htmlentities($result->SpotLocation);?></h4>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<a href="spot-details.php?pkgid=<?php echo htmlentities($result->SpotId);?>" class="view">Details</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php }} ?>
+			
+		
+<div></div>
+</div>
+			<div class="clearfix"></div>
+	</div>
+
+
+
+<!--- routes ---->
+<div class="routes">
+	<div class="container">
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="glyphicon glyphicon-list-alt"></i></a>
+			</div>
+			<div class="rou-rgt wow fadeInDown animated" data-wow-delay=".5s">
+				<!--<h3>80000</h3>
+				<p>Enquiries</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-user"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>1900</h3>
+				<p>Regestered users</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-ticket"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>7,00,00,000+</h3>
+				<p>Booking</p>
+			</div>-->
+				<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
